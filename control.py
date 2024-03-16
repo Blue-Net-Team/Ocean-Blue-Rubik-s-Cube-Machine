5cae1c3b3a174d57d6dfdcee4457525b4ea82160
IVEN_He <13377529851@qq.com>
2024-03-16 12:49:59 +0800

    添加机械臂的类

--- control.py
+++ control.py
@@ -59,6 +59,36 @@ class ClampCylinder:
         GPIO.output(self.pin, GPIO.LOW)
 
 
+class Arm:
+    """机械臂"""
+    def __init__(self, clamp:ClampCylinder, motor:SteppingMotor) -> None:
+        """机械臂的初始化
+        ----
+        * clamp: 手臂的夹爪
+        * motor: 步进电机"""
+        self.clamp:ClampCylinder = clamp
+        self.motor:SteppingMotor = motor
+
+    def close_spin(self, num:int, reverse=False):
+        """机械臂闭合并旋转 -> 转动魔方
+        ----
+        * num: 旋转的圈数
+        * reverse: 是否反向旋转，默认为False(顺时旋转)"""
+        self.clamp.close()
+        self.motor.move(num, reverse)
+
+    def open_spin(self, num:int, reverse=False):
+        """机械臂张开并旋转 -> 手指归位
+        ----
+        * num: 旋转的圈数
+        * reverse: 是否反向旋转，默认为False(顺时旋转)"""
+        self.clamp.open()
+        self.motor.move(num, reverse)
+
+    def open(self):
+        """手指张开"""
+        self.clamp.open()
+
 if __name__ == '__main__':
     motor = SteppingMotor(27, 22, 17)
     motor.move(1)
