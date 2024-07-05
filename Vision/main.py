8ec10667750deda893c86427a28cbffe6a5f71c1
IVEN_He <13377529851@qq.com>
2024-07-05 21:07:56 +0800

    在main文件使用多进程并发操作，有效提高识别速度

--- Vision/main.py
+++ Vision/main.py
@@ -2,36 +2,35 @@ import cube_solver
 import time
 import communication
 import arm_planning
-import twophase.cubie as cubie
+import cam
+from multiprocessing import Pool
 
-from cam import Right
-from cam import Up
-from cam import Left
-from cam import Down
+Ucam = cam.UpCam()
+Dcam = cam.DownCam()
+Lcam = cam.LeftCam()
+Rcam = cam.RightCam()
 
+ifio:bool=True
+
+def detect_color(side:cam.Cam):
+    return side.detect_color(ifio)
 
 def crack():
     # 获取魔方颜色状态
-    print('U')
-    [color_state2,color_state3] = Up.detect_color()
-    print('R')
-    color_state1 = Right.detect_color()
-    print('L')
-    color_state4 = Left.detect_color()
-    print('D')
-    [color_state5,color_state6] = Down.detect_color()
+    sides:list[cam.Cam] = [Ucam, Rcam, Lcam, Dcam]
+    t1 = time.perf_counter()
+    with Pool(4) as p:
+        res0 = p.map(detect_color, sides)
+    t2 = time.perf_counter()
+    print('detect time:',t2-t1)
+
+    color_state2,color_state3 = res0[0]
+    color_state1 = res0[1]
+    color_state4 = res0[2]
+    color_state5,color_state6 = res0[3]
     color_state = color_state3 + color_state1 + color_state2 + color_state6 + color_state4 + color_state5
     color_state = cube_solver.color2view(color_state)
     print("color state is:" + color_state)
-
-    # region 生成随机魔方状态用于测试机械
-    # cc = cubie.CubieCube()
-    # cnt = [0] * 31
-    # cc.randomize()
-    # fc = cc.to_facelet_cube()
-    # s = fc.to_string()
-    # color_state = s
-    # endregion
 
     # 解算得到机械臂步骤
     solve_step = cube_solver.cube_solver(color_state)
@@ -39,6 +38,7 @@ def crack():
     _solve_step = _solve_step[:-1]
     print("the orign step is:", _solve_step)
     real_solve, cost = cube_solver._SolutionTransAndOptimize(_solve_step)
+    # real_solve = _solve_step
     arm_step = arm_planning.planning(real_solve)  # ['RO', 'L2', 'RC', 'R2']
     print("--------------------------------------------")
     print("the optimize step is:",real_solve)
