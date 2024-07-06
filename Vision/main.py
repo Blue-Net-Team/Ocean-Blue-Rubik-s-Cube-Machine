69642177f3279aecf0ed838acc735894c61ac578
IVEN_He <13377529851@qq.com>
2024-07-06 13:47:06 +0800

    主文件中为更改了并发逻辑，将图片传入进程池，解决摄像头不可序列化的问题

--- Vision/main.py
+++ Vision/main.py
@@ -4,6 +4,7 @@ import communication
 import arm_planning
 import cam
 from multiprocessing import Pool
+import cv2
 
 Ucam = cam.UpCam()
 Dcam = cam.DownCam()
@@ -12,23 +13,38 @@ Rcam = cam.RightCam()
 
 ifio:bool=True
 
-def detect_color(side:cam.Cam):
-    return side.detect_color(ifio)
+def main_detect_color(img:cv2.typing.MatLike, side:str):
+    if side not in ['U','D','L','R']:
+        raise ValueError('side should be one of U,D,L,R')
+    if side == 'U':
+        return Ucam.detect_color(img, ifio)
+    elif side == 'D': 
+        return Dcam.detect_color(img, ifio)
+    elif side == 'L': 
+        return Lcam.detect_color(img, ifio)
+    elif side == 'R': 
+        return Rcam.detect_color(img, ifio)
+    
 
 def crack():
     # 获取魔方颜色状态
-    sides:list[cam.Cam] = [Ucam, Rcam, Lcam, Dcam]
+    Limg = Lcam.read_usb_capture()
+    Rimg = Rcam.read_usb_capture()
+    Uimg = Ucam.read_usb_capture()
+    Dimg = Dcam.read_usb_capture()
+
+    sides:list[tuple[cv2.typing.MatLike,str]] = [(Uimg,'U'),(Rimg,'R'),(Limg,'L'),(Dimg,'D')]
     t1 = time.perf_counter()
     with Pool(4) as p:
-        res0 = p.map(detect_color, sides)
+        res0 = p.starmap(main_detect_color, sides)
     t2 = time.perf_counter()
     print('detect time:',t2-t1)
 
-    color_state2,color_state3 = res0[0]
+    color_state2,color_state3 = res0[0]     # type:ignore
     color_state1 = res0[1]
     color_state4 = res0[2]
-    color_state5,color_state6 = res0[3]
-    color_state = color_state3 + color_state1 + color_state2 + color_state6 + color_state4 + color_state5
+    color_state5,color_state6 = res0[3]     # type:ignore
+    color_state = color_state3 + color_state1 + color_state2 + color_state6 + color_state4 + color_state5   # type:ignore
     color_state = cube_solver.color2view(color_state)
     print("color state is:" + color_state)
 
